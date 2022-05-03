b8273bacc5d381b504fb41204717dacfb0b75bdf
restato <direcision@gmail.com>
2022-05-03 21:51:05 +0900

    Init transaction constructor

--- app/lib/transaction.dart
+++ app/lib/transaction.dart
@@ -1,13 +1,45 @@
 class Transaction {
+  final String amount;
+  final String yearOfConstruction;
+  final String type;
+  final String dong;
+  final String aptName;
   final String year;
+  final String month;
+  final String day;
+  final String dedicatedArea;
+  final String jibun;
+  final String areaCode;
+  final String floor;
 
-  const Transaction({
-    required this.year,
-  });
+  const Transaction(
+      {required this.amount,
+      required this.yearOfConstruction,
+      required this.type,
+      required this.dong,
+      required this.aptName,
+      required this.year,
+      required this.month,
+      required this.day,
+      required this.dedicatedArea,
+      required this.jibun,
+      required this.areaCode,
+      required this.floor});
 
   factory Transaction.fromJson(Map<String, dynamic> json) {
     return Transaction(
-      year: json['거래금액'],
+      amount: json['거래금액'],
+      yearOfConstruction: json['건축년도'],
+      type: json['거래유형'],
+      dong: json['법정동'],
+      aptName: json['아파트'],
+      year: json['년'],
+      month: json['월'],
+      day: json['일'],
+      dedicatedArea: json['전용면적'],
+      jibun: json['지번'],
+      areaCode: json['지역코드'],
+      floor: json['층'],
     );
   }
 }
